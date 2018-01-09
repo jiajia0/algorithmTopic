77d3c64a81a15cdf4423ce28bb08f8d1197736f2
leafage <majialongcode@gmail.com>
2018-01-09 13:19:33 +0800

    Update

--- LeetCode-CPP/Array/287. Find the Duplicate Number.cpp
+++ LeetCode-CPP/Array/287. Find the Duplicate Number.cpp
@@ -16,7 +16,7 @@ int main() {
     Solution s;
     vector<int> num;
     num.push_back(2);
-    num.push_back(0);
+    num.push_back(2);
     num.push_back(1);
     int ret = s.findDuplicate(num);
     cout << ret;
