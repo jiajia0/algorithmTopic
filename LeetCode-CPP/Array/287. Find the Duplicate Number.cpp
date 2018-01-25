96f85da86bd794da2f5b055efb6e9943e01f0f0c
leafage <majialongcode@gmail.com>
2018-01-25 23:29:53 +0800

    287. Find the Duplicate Number

--- LeetCode-CPP/Array/287. Find the Duplicate Number.cpp
+++ LeetCode-CPP/Array/287. Find the Duplicate Number.cpp
@@ -5,19 +5,50 @@
 
 using namespace std;
 
+/*class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        for(int i=0;i<nums.size();i++) {
+            int n = abs(nums[i]);
+            if(nums[n] < 0) {
+                return abs(nums[i]);
+            } else {
+                nums[n] *= -1;
+            }
+        }
+        return -1;
+    }
+};*/
+
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
+        int tortoise = nums[0];
+        int hare = nums[0];
 
+        do {
+            //cout << tortoise << "," << hare << endl;
+            tortoise = nums[tortoise];//慢的走一步
+            hare = nums[nums[hare]];//快的走两步
+        } while(tortoise != hare);
+
+        int ptr1 = nums[0];
+        int ptr2 = tortoise;
+
+        while(ptr1 != ptr2) {
+            //cout << ptr1 << "," << ptr2 << endl;
+            ptr1 = nums[ptr1];
+            ptr2 = nums[ptr2];
+        }
+
+        return ptr1;
     }
 };
 
 int main() {
     Solution s;
-    vector<int> num;
-    num.push_back(2);
-    num.push_back(2);
-    num.push_back(1);
+    vector<int> num = {2,5,9,6,9,3,8,9,7,1};
+
     int ret = s.findDuplicate(num);
     cout << ret;
 }
