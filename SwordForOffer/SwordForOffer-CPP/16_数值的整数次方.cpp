edbcaf6ac3370dc6113281dcd1bdb28591e392db
jiajia <majialongcode@gmail.com>
2018-03-01 22:42:51 +0800

    SwordForOffer-CPP

---
+++ SwordForOffer/SwordForOffer-CPP/16_数值的整数次方.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+using namespace std;
+
+// 面试题16：数值的整数次方
+// 题目：实现函数double Power(double base, int exponent)，求base的exponent
+// 次方。不得使用库函数，同时不需要考虑大数问题。
+
+bool equal(double num1, double num2);
+double PowerWithUnsignedExponent(double base, unsigned int exponent);
+
+
+bool g_InvalidInput = false;
+
+double Power(double base, int exponent) {
+    g_InvalidInput = false;
+
+    // 如果底数为0，并且指数小于0，抛出异常
+    if(equal(base,0.0) && exponent < 0) {
+        g_InvalidInput = true;
+        return 0.0;
+    }
+
+    unsigned int absExponent = (unsigned int)(exponent);
+    // 指数小于零时，将其转换为正数
+    if(exponent < 0)
+        absExponent = (unsigned int)(-exponent);
+
+    // 计算出结果
+    double result = PowerWithUnsignedExponent(base,absExponent);
+
+    // 如果是负数，则需要求倒数
+    if(exponent < 0)
+        result = 1.0/result;
+
+    return result;
+}
+
+double PowerWithUnsignedExponent(double base, unsigned int exponent) {
+    if(exponent == 0)
+        return 1;
+    if(exponent == 1)
+        return base;
+
+    // 求a^(n/2)
+    double result = PowerWithUnsignedExponent(base,exponent >> 1);
+    // 平方
+    result *= result;
+    // 判断奇数还是偶数
+    if(exponent & 0x1 == 1)
+        result *= base;
+    return result;
+}
+
+/*double PowerWithUnsignedExponent(double base, unsigned int exponent) {
+    double result = 1.0;
+    for(int i=1; i <= exponent; i++)
+        result *= base;
+    return result;
+}*/
+
+bool equal(double num1, double num2)
+{
+    if ((num1 - num2 > -0.0000001) && (num1 - num2 < 0.0000001))
+        return true;
+    else
+        return false;
+}
+
+// ====================测试代码====================
+void Test(const char* testName, double base, int exponent, double expectedResult, bool expectedFlag)
+{
+    double result = Power(base, exponent);
+    if (equal(result, expectedResult) && g_InvalidInput == expectedFlag)
+        std::cout << testName << " passed" << std::endl;
+    else
+        std::cout << testName << " FAILED" << std::endl;
+}
+
+int main(int argc, char* argv[])
+{
+    // 底数、指数都为正数
+    Test("Test1", 2, 3, 8, false);
+
+    // 底数为负数、指数为正数
+    Test("Test2", -2, 3, -8, false);
+
+    // 指数为负数
+    Test("Test3", 2, -3, 0.125, false);
+
+    // 指数为0
+    Test("Test4", 2, 0, 1, false);
+
+    // 底数、指数都为0
+    Test("Test5", 0, 0, 1, false);
+
+    // 底数为0、指数为正数
+    Test("Test6", 0, 4, 0, false);
+
+    // 底数为0、指数为负数
+    Test("Test7", 0, -4, 0, true);
+
+    return 0;
+}
+
