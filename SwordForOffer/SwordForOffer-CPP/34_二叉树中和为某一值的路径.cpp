e65504f134df44bdd0c1ecce353ccc263d6d78e7
jiajia <majialongcode@gmail.com>
2018-03-17 16:38:02 +0800

    update by 2018.3.17

---
+++ SwordForOffer/SwordForOffer-CPP/34_二叉树中和为某一值的路径.cpp
@@ -0,0 +1,195 @@
+#include <iostream>
+#include <vector>
+#include "Utils/BinaryTree.h"
+
+using namespace std;
+
+// 面试题34：二叉树中和为某一值的路径
+// 题目：输入一棵二叉树和一个整数，打印出二叉树中结点值的和为输入整数的所
+// 有路径。从树的根结点开始往下一直到叶结点所经过的结点形成一条路径。
+
+void FindPath(BinaryTreeNode* pRoot, int expectedSum, vector<BinaryTreeNode*> path,int currentSum);
+
+void FindPath(BinaryTreeNode* pRoot, int expectedSum) {
+    if(pRoot == nullptr)
+        return;
+
+    // 初始化数据
+    vector<BinaryTreeNode*> path;// 用来保存访问过的路径
+    int curretnSum = 0;// 用来保存访问过路径的数据和
+    FindPath(pRoot,expectedSum,path,curretnSum);
+}
+
+void FindPath(BinaryTreeNode* pRoot, int expectedSum, vector<BinaryTreeNode*> path,int currentSum) {
+
+    currentSum += pRoot->m_nValue;// 首先当前的和加上根节点的和
+
+    if(currentSum > expectedSum)
+        return;
+
+    path.push_back(pRoot);// 将该节点放入到已经访问过的路劲中
+
+    // 如果是叶子节点，并且路径中的和等于期望中的和，就将他们打印出来
+    bool isLeaf = pRoot->m_pLeft == nullptr && pRoot->m_pRight == nullptr;
+    if(currentSum == expectedSum && isLeaf) {
+        cout << "A path is found:";
+        for(BinaryTreeNode* item : path)
+            cout << item->m_nValue << "\t";
+        cout << "\n";
+    }
+
+    // 如果还有子节点，则去查找子节点的信息
+    if(pRoot->m_pLeft != nullptr)
+        FindPath(pRoot->m_pLeft,expectedSum,path,currentSum);
+    if(pRoot->m_pRight != nullptr)
+        FindPath(pRoot->m_pRight,expectedSum,path,currentSum);
+
+    path.pop_back();// 在返回父节点之前需要将当前节点从路径中删除
+    currentSum -= pRoot->m_nValue;// 减去当前节点计算的数值
+}
+
+
+// ====================测试代码====================
+void Test(char* testName, BinaryTreeNode* pRoot, int expectedSum)
+{
+    if(testName != nullptr)
+        printf("%s begins:\n", testName);
+
+    FindPath(pRoot, expectedSum);
+
+    printf("\n");
+}
+
+//            10
+//         /      \
+//        5        12
+//       /\
+//      4  7
+// 有两条路径上的结点和为22
+void Test1()
+{
+    BinaryTreeNode* pNode10 = CreateBinaryTreeNode(10);
+    BinaryTreeNode* pNode5 = CreateBinaryTreeNode(5);
+    BinaryTreeNode* pNode12 = CreateBinaryTreeNode(12);
+    BinaryTreeNode* pNode4 = CreateBinaryTreeNode(4);
+    BinaryTreeNode* pNode7 = CreateBinaryTreeNode(7);
+
+    ConnectTreeNodes(pNode10, pNode5, pNode12);
+    ConnectTreeNodes(pNode5, pNode4, pNode7);
+
+    printf("Two paths should be found in Test1.\n");
+    Test("Test1", pNode10, 22);
+
+    DestroyTree(pNode10);
+}
+
+//            10
+//         /      \
+//        5        12
+//       /\
+//      4  7
+// 没有路径上的结点和为15
+void Test2()
+{
+    BinaryTreeNode* pNode10 = CreateBinaryTreeNode(10);
+    BinaryTreeNode* pNode5 = CreateBinaryTreeNode(5);
+    BinaryTreeNode* pNode12 = CreateBinaryTreeNode(12);
+    BinaryTreeNode* pNode4 = CreateBinaryTreeNode(4);
+    BinaryTreeNode* pNode7 = CreateBinaryTreeNode(7);
+
+    ConnectTreeNodes(pNode10, pNode5, pNode12);
+    ConnectTreeNodes(pNode5, pNode4, pNode7);
+
+    printf("No paths should be found in Test2.\n");
+    Test("Test2", pNode10, 15);
+
+    DestroyTree(pNode10);
+}
+
+//               5
+//              /
+//             4
+//            /
+//           3
+//          /
+//         2
+//        /
+//       1
+// 有一条路径上面的结点和为15
+void Test3()
+{
+    BinaryTreeNode* pNode5 = CreateBinaryTreeNode(5);
+    BinaryTreeNode* pNode4 = CreateBinaryTreeNode(4);
+    BinaryTreeNode* pNode3 = CreateBinaryTreeNode(3);
+    BinaryTreeNode* pNode2 = CreateBinaryTreeNode(2);
+    BinaryTreeNode* pNode1 = CreateBinaryTreeNode(1);
+
+    ConnectTreeNodes(pNode5, pNode4, nullptr);
+    ConnectTreeNodes(pNode4, pNode3, nullptr);
+    ConnectTreeNodes(pNode3, pNode2, nullptr);
+    ConnectTreeNodes(pNode2, pNode1, nullptr);
+
+    printf("One path should be found in Test3.\n");
+    Test("Test3", pNode5, 15);
+
+    DestroyTree(pNode5);
+}
+
+// 1
+//  \
+//   2
+//    \
+//     3
+//      \
+//       4
+//        \
+//         5
+// 没有路径上面的结点和为16
+void Test4()
+{
+    BinaryTreeNode* pNode1 = CreateBinaryTreeNode(1);
+    BinaryTreeNode* pNode2 = CreateBinaryTreeNode(2);
+    BinaryTreeNode* pNode3 = CreateBinaryTreeNode(3);
+    BinaryTreeNode* pNode4 = CreateBinaryTreeNode(4);
+    BinaryTreeNode* pNode5 = CreateBinaryTreeNode(5);
+
+    ConnectTreeNodes(pNode1, nullptr, pNode2);
+    ConnectTreeNodes(pNode2, nullptr, pNode3);
+    ConnectTreeNodes(pNode3, nullptr, pNode4);
+    ConnectTreeNodes(pNode4, nullptr, pNode5);
+
+    printf("No paths should be found in Test4.\n");
+    Test("Test4", pNode1, 16);
+
+    DestroyTree(pNode1);
+}
+
+// 树中只有1个结点
+void Test5()
+{
+    BinaryTreeNode* pNode1 = CreateBinaryTreeNode(1);
+
+    printf("One path should be found in Test5.\n");
+    Test("Test5", pNode1, 1);
+
+    DestroyTree(pNode1);
+}
+
+// 树中没有结点
+void Test6()
+{
+    printf("No paths should be found in Test6.\n");
+    Test("Test6", nullptr, 0);
+}
+
+int main(int argc, char* argv[])
+{
+    Test1();
+    Test2();
+    Test3();
+    Test4();
+    Test5();
+    Test6();
+
+    return 0;
+}
