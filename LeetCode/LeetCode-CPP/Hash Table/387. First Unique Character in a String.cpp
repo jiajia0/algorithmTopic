c4358c022b00b8d6b61d03e91aefa6fa7d6b5f73
jiajia <majialongcode@gmail.com>
2019-07-15 22:55:00 +0800

    387. First Unique Character in a String

--- LeetCode/LeetCode-CPP/Hash Table/387. First Unique Character in a String.cpp
+++ LeetCode/LeetCode-CPP/Hash Table/387. First Unique Character in a String.cpp
@@ -23,6 +23,20 @@ public:
     }
 };
 
+/*class Solution {
+public:
+    int firstUniqChar(string s) {
+        map<char,int> count;
+        for(char ch : s)
+            count[ch]++;
+        for(int i=0;i<s.size();i++) {
+            if(count[s[i]] == 1)
+                return i;
+        }
+        return -1;
+    }
+};*/
+
 int main() {
     Solution s;
     string s1 = "leetcodetlcod";
